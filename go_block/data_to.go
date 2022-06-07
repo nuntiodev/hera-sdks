804f4dc4b2b98bd9bb3bebf801c8c13b9db6d113
Oscar A. B. Orellana <oscar@softcorp.io>
2022-06-07 10:00:27 +0200

    updated

--- go_block/data_to.go
+++ go_block/data_to.go
@@ -10,10 +10,10 @@ func (x *User) DataTo(to interface{}) error {
 	if to == nil {
 		return errors.New("cannot unpack value to nil")
 	}
-	if x.Metadata == nil && strings.TrimSpace(x.Metadata.Body) == "" {
+	if strings.TrimSpace(x.Metadata) == "" {
 		return nil
 	} else {
-		if err := json.Unmarshal([]byte(x.Metadata.Body), to); err != nil {
+		if err := json.Unmarshal([]byte(x.Metadata), to); err != nil {
 			return err
 		}
 	}
