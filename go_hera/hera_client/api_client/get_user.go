ac5e7a9e27095f5e21dcfe0b00e632920f564604
Oscar A. B. Orellana <oscar@softcorp.io>
2022-08-01 11:30:24 +0200

    updated

--- go_hera/hera_client/api_client/get_user.go
+++ go_hera/hera_client/api_client/get_user.go
@@ -2,6 +2,9 @@ package api_client
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/nuntiodev/x/pointerx"
+	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/nuntiodev/hera-sdks/go_hera"
 	"github.com/nuntiodev/hera-sdks/go_hera/hera_client/hera_options"
@@ -9,10 +12,16 @@ import (
 )
 
 type GetUserRequest struct {
-	findOptions *hera_options.FindOptions
-	namespace   string
-	client      go_hera.ServiceClient
-	authorize   cloud_authorize.CloudAuthorize
+	searchMetadata bson.M
+	findOptions    *hera_options.FindOptions
+	namespace      string
+	client         go_hera.ServiceClient
+	authorize      cloud_authorize.CloudAuthorize
+}
+
+func (r *GetUserRequest) SetSearchMetadata(filter bson.M) *GetUserRequest {
+	r.searchMetadata = filter
+	return r
 }
 
 func (r *GetUserRequest) Execute(ctx context.Context) (*go_hera.User, error) {
@@ -31,10 +40,21 @@ func (r *GetUserRequest) Execute(ctx context.Context) (*go_hera.User, error) {
 		Username: r.findOptions.Username,
 		Phone:    r.findOptions.Phone,
 	}
+	var query *go_hera.Query
+	if r.searchMetadata != nil {
+		m, err := json.Marshal(r.searchMetadata)
+		if err != nil {
+			return nil, err
+		}
+		query = &go_hera.Query{
+			SearchMetadata: pointerx.StringPtr(string(m)),
+		}
+	}
 	userResp, err := r.client.GetUser(ctx, &go_hera.HeraRequest{
 		CloudToken: accessToken,
 		User:       getUser,
 		Namespace:  r.namespace,
+		Query:      query,
 	})
 	if err != nil {
 		return nil, err
--- go_hera/hera_client/api_client/list_users.go
+++ go_hera/hera_client/api_client/list_users.go
@@ -2,6 +2,8 @@ package api_client
 
 import (
 	"context"
+	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/nuntiodev/hera-sdks/go_hera"
 	"github.com/nuntiodev/hera-sdks/go_hera/hera_client/hera_options"
@@ -9,10 +11,16 @@ import (
 )
 
 type ListUsersRequest struct {
-	namespace     string
-	encryptionKey string
-	client        go_hera.ServiceClient
-	authorize     cloud_authorize.CloudAuthorize
+	searchMetadata bson.M
+	namespace      string
+	encryptionKey  string
+	client         go_hera.ServiceClient
+	authorize      cloud_authorize.CloudAuthorize
+}
+
+func (r *ListUsersRequest) SetSearchMetadata(filter bson.M) *ListUsersRequest {
+	r.searchMetadata = filter
+	return r
 }
 
 func (r *ListUsersRequest) Execute(ctx context.Context, queryOptions *hera_options.QueryOptions) ([]*go_hera.User, error) {
@@ -23,12 +31,21 @@ func (r *ListUsersRequest) Execute(ctx context.Context, queryOptions *hera_optio
 	if queryOptions == nil {
 		queryOptions = &hera_options.QueryOptions{}
 	}
+	var searchMetadata string
+	if r.searchMetadata != nil {
+		m, err := json.Marshal(r.searchMetadata)
+		if err != nil {
+			return nil, err
+		}
+		searchMetadata = string(m)
+	}
 	userResp, err := r.client.ListUsers(ctx, &go_hera.HeraRequest{
 		CloudToken: accessToken,
 		Namespace:  r.namespace,
 		Query: &go_hera.Query{
-			From: queryOptions.From,
-			To:   queryOptions.To,
+			From:           queryOptions.From,
+			To:             queryOptions.To,
+			SearchMetadata: &searchMetadata,
 		},
 	})
 	if err != nil {
